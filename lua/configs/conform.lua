f57c78bf5bfc5ae21f265467fbd2fe1d06934292
adamthedash <adamthedash2@gmail.com>
2025-02-18 20:30:18 +0000

    fix ruff integration

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,8 +1,15 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    python = { "ruff" },
-    rust = {"rust_analyzer"},
+    python = {
+      -- To fix auto-fixable lint errors.
+      "ruff_fix",
+      -- To run the Ruff formatter.
+      "ruff_format",
+      -- To organize the imports.
+      "ruff_organize_imports",
+    },
+    rust = { "rust_analyzer" },
   },
 
   format_on_save = {
