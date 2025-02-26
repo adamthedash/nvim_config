30182ed5563c69f54060fd548dab89b6e8b69370
adamthedash <adamthedash2@gmail.com>
2025-02-26 19:37:45 +0000

    Add Typescript lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "ruff", "pyright" }
+local servers = { "html", "cssls", "ruff", "pyright", "svelte", "ts_ls"}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
