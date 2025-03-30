0b41d7f7df7eff258ae5e170f0e962e0e6f7d8ca
adamthedash <adamthedash2@gmail.com>
2025-03-30 23:34:16 +0100

    Add leptosfmt

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -9,14 +9,19 @@ local options = {
       -- To organize the imports.
       "ruff_organize_imports",
     },
-    rust = { "rust_analyzer" },
+    rust = { "rust_analyzer", "leptosfmt" },
   },
-
   format_on_save = {
     -- These options will be passed to conform.format()
     timeout_ms = 500,
     lsp_fallback = true,
   },
+  formatters = {
+    leptosfmt = {
+      command = "leptosfmt",
+      args = { "--stdin" },
+      stdin = true,
+    },
+  },
 }
-
 return options
